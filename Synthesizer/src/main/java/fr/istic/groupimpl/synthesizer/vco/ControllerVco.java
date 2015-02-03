04a99917d49865d967b40202e9af4f74f36cfc67
hacaultrobin <hacault.robin@free.fr>
2015-02-03 18:13:45 +0100

    ModelVCO : Init circuit Jsyn

--- Synthesizer/src/main/java/fr/istic/groupimpl/synthesizer/vco/ControllerVco.java
+++ Synthesizer/src/main/java/fr/istic/groupimpl/synthesizer/vco/ControllerVco.java
@@ -1,5 +1,6 @@
 package fr.istic.groupimpl.synthesizer.vco;
 
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 import fr.istic.groupimpl.synthesizer.component.IControllerComponent;
 
 public class ControllerVco implements IControllerComponent {
@@ -15,11 +16,31 @@ public class ControllerVco implements IControllerComponent {
 	
 	/* =================== Event handles FROM the view TO the model =================== */
 	
-	public void handleViewOctaveChange(double octave, double precision) {
-		System.err.println("Octave (view) Potentiomètre changed : " + octave + " / " + precision);
-		modelVco.setValProperty("octave", octave + precision);
-		modelVco.setJsynOctave(octave + precision);
+	@Override
+	public void handleViewInputClick(String portName) {
+		// TODO Auto-generated method stub
+		throw new NotImplementedException();
 	}
+
+
+	@Override
+	public void handleViewOutpuClick(String portName) {
+		// TODO Auto-generated method stub
+		throw new NotImplementedException();
+	}
+	
+	public void handleViewOctaveChange(double octave, double precision) {
+		double realOctave = octave;
+		// Test precision : No negative values
+		if (octave >= 1.0 || (octave <= 1.0 && precision >= 0.0)) {
+			realOctave+= precision;
+		}
+		System.err.println("Octave (view) Potentiomètre changed : " + realOctave);
+		modelVco.setValProperty("octave", realOctave);
+		modelVco.setJsynOctave(realOctave);
+	}
+	
+	
 	
 	
 	/* =================== Event handles FROM the model TO the view =================== */
