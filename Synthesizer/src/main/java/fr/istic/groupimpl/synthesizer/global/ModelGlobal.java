04e8213be471dff8cd77f617d46e53f7ee6175d4
Dorian Leroy <leroy.dorian@yahoo.fr>
2015-02-03 10:55:44 +0100

    Added utility methods to disconnect ports in ModelGlobal

--- Synthesizer/src/main/java/fr/istic/groupimpl/synthesizer/global/ModelGlobal.java
+++ Synthesizer/src/main/java/fr/istic/groupimpl/synthesizer/global/ModelGlobal.java
@@ -45,30 +45,66 @@ public class ModelGlobal {
 		modules.remove(model);
 	}
 
-
-
 	public void connectModules(IModelComponent moduleOut, String outputPort, IModelComponent moduleIn, String inputPort) {
-		UnitOutputPort out = (UnitOutputPort) moduleIn.getUnitGenerator().getPortByName(outputPort);
+		UnitOutputPort out = (UnitOutputPort) moduleOut.getUnitGenerator().getPortByName(outputPort);
 		UnitInputPort in = (UnitInputPort) moduleIn.getUnitGenerator().getPortByName(inputPort);
 		out.connect(in);
 		putOutputConnexion(moduleOut, outputPort, moduleIn, inputPort);
 		putInputConnexion(moduleIn, inputPort, moduleOut, outputPort);
 		controller.handleConnectModules();
 	}
-
+	
 	public void disconnectModules(IModelComponent moduleOut, String outputPort, IModelComponent moduleIn, String inputPort) {
-		UnitOutputPort out = (UnitOutputPort) moduleIn.getUnitGenerator().getPortByName(outputPort);
+		UnitOutputPort out = (UnitOutputPort) moduleOut.getUnitGenerator().getPortByName(outputPort);
 		UnitInputPort in = (UnitInputPort) moduleIn.getUnitGenerator().getPortByName(inputPort);
 		out.disconnect(in);
 		
 	}
 	
+	/*
+	 * Called by the controller whenever a cable is disconnected but not deleted.
+	 * Disconnects the output port connected to the given input port, if any.
+	 */
 	public void disconnectInputPort(IModelComponent module, String port) {
-//		
-//		UnitOutputPort out = (UnitOutputPort) moduleIn.getUnitGenerator().getPortByName(outputPort);
-//		UnitInputPort in = (UnitInputPort) moduleIn.getUnitGenerator().getPortByName(inputPort);
-//		out.disconnect(in);
-		
+		Map<String,Pair<IModelComponent,String>> tmp = inputConnexions.get(module);
+		if(tmp != null) {
+			Pair<IModelComponent,String> p = tmp.get(port);
+			UnitOutputPort out = (UnitOutputPort) p.getKey().getUnitGenerator().getPortByName(p.getValue());
+			UnitInputPort in = (UnitInputPort) module.getUnitGenerator().getPortByName(port);
+			out.disconnect(in);
+			tmp.remove(port);
+			inputConnexions.put(module, tmp);
+		}
+	}
+	
+	public void disconnectAllInputPort(IModelComponent module) {
+		Map<String,Pair<IModelComponent,String>> tmp = inputConnexions.get(module);
+		if(tmp != null) {
+			tmp.forEach((s,p) -> {
+				UnitOutputPort out = (UnitOutputPort) p.getKey().getUnitGenerator().getPortByName(p.getValue());
+				UnitInputPort in = (UnitInputPort) module.getUnitGenerator().getPortByName(s);
+				out.disconnect(in);
+			});
+			inputConnexions.remove(module);
+		}
+	}
+	
+	/*
+	 * Called by the controller whenever a cable is disconnected but not deleted.
+	 */
+	public void disconnectOutputPort(IModelComponent module, String port) {
+		/*
+		 * Retrieve all connexions from this module
+		 */
+		Map<String,Pair<IModelComponent,String>> tmp = outputConnexions.get(module);
+		if(tmp != null) {
+			Pair<IModelComponent,String> p = tmp.get(port);
+			UnitOutputPort out = (UnitOutputPort) module.getUnitGenerator().getPortByName(port);
+			UnitInputPort in = (UnitInputPort) p.getKey().getUnitGenerator().getPortByName(p.getValue());
+			out.disconnect(in);
+			tmp.remove(port);
+			outputConnexions.put(module, tmp);
+		}
 	}
 	
 	private void putOutputConnexion(IModelComponent moduleOut, String outputPort, IModelComponent moduleIn, String inputPort) {
