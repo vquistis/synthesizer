8e12f1b49a68765bc25a9e976e8a4efed705e6c1
Anthony LHOMME <lhomme.anthony@gmail.com>
2015-02-04 10:49:33 +0100

    OUT : correction bind entre text field et knob volume

--- Synthesizer/src/main/java/fr/istic/groupimpl/synthesizer/out/ViewOut.java
+++ Synthesizer/src/main/java/fr/istic/groupimpl/synthesizer/out/ViewOut.java
@@ -4,10 +4,6 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 import javafx.beans.binding.Bindings;
-import javafx.beans.property.DoubleProperty;
-import javafx.beans.property.SimpleDoubleProperty;
-import javafx.beans.property.StringProperty;
-import javafx.beans.value.WritableValue;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.CheckBox;
@@ -16,8 +12,8 @@ import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.VBox;
 import javafx.util.StringConverter;
-import javafx.util.converter.NumberStringConverter;
 import fr.istic.groupimpl.synthesizer.component.IViewComponent;
+import fr.istic.groupimpl.synthesizer.util.DoubleStringConverter;
 import fr.istic.groupimpl.synthesizer.util.Potentiometre;
 import fr.istic.groupimpl.synthesizer.util.PotentiometreFactory;
 
@@ -28,52 +24,6 @@ public class ViewOut implements IViewComponent, Initializable {
 	@FXML private TextField valueVolumeFx;
 	@FXML private CheckBox muteVolumeFx;
 	@FXML private ImageView input;
-	
-	/**
-	 * Converter -12 / +12 to -Inf / +12
-	 * @param val - value between -12db and +12db
-	 * @return value between -Inf and +12db
-	 */
-	private Double convVal(Double val){
-		if (val >= 0) {
-			return val;
-		}
-		if (val < -11.9) {
-			val = -11.9;
-		}
-		return (-1./(12.+val)+1./12.)*132 ;
-	}
-	private DoubleProperty knobInfValue = new SimpleDoubleProperty();
-	private class StringConverterInf extends StringConverter<Number> {
-
-		final private String STR_INF = "-inf";
-		final private double DOUBLE_INF = -1000.;
-
-		StringConverter<Number> defConverter = new NumberStringConverter();
-
-		@Override
-		public String toString(Number object) {
-
-			if (object instanceof Double) {
-				double d = (Double) object;
-				if (d <= DOUBLE_INF) {
-					return STR_INF;
-				}
-			}
-			return defConverter.toString(object);
-
-		}
-
-		@Override
-		public Number fromString(String string) {
-
-			if (string.equals(STR_INF)) {
-				return -1000.;
-			}
-			return defConverter.fromString(string);
-		}
-
-	}
 
 	@Override
 	public void initialize(URL location, ResourceBundle resource) {
@@ -83,19 +33,10 @@ public class ViewOut implements IViewComponent, Initializable {
 		knobFact.setValueDef(0);
 		knobFact.setNbSpins(3);
 		Potentiometre knobVolume = knobFact.getPotentiometre();
-		//volumeKnob.valueProperty().addListener((obsVal, oldVal, newVal) -> knobInfValue.set(convVal((Double)newVal)));
 		knobVolumePane.getChildren().add(knobVolume);
 		
-		valueVolumeFx.textProperty().addListener((observable, oldValue, newValue) -> {
-			try {
-				Integer.valueOf(newValue);
-			} catch (Exception e) {
-				((StringProperty) observable).setValue(oldValue);
-			}
-		});
-		
 		// Bind knob value and text field value
-		StringConverter<Number> converter = new NumberStringConverter();
+		StringConverter<Number> converter = new DoubleStringConverter();
 		Bindings.bindBidirectional(valueVolumeFx.textProperty(), knobVolume.valueProperty(), converter);
 
 		// Creation du controller
