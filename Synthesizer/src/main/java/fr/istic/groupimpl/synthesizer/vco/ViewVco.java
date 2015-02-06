00179f9837fa3391b7415b2e6a93c876a5210c4d
hacaultrobin <hacault.robin@free.fr>
2015-02-06 10:27:14 +0100

    View VCO - Remove unused imports

--- Synthesizer/src/main/java/fr/istic/groupimpl/synthesizer/vco/ViewVco.java
+++ Synthesizer/src/main/java/fr/istic/groupimpl/synthesizer/vco/ViewVco.java
@@ -7,7 +7,6 @@ import javafx.beans.property.DoubleProperty;
 import javafx.beans.property.SimpleDoubleProperty;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.geometry.Bounds;
 import javafx.scene.control.Label;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.ToggleGroup;
@@ -21,7 +20,6 @@ import javafx.scene.text.Text;
 import fr.istic.groupimpl.synthesizer.component.ViewComponent;
 import fr.istic.groupimpl.synthesizer.util.Potentiometre;
 import fr.istic.groupimpl.synthesizer.util.PotentiometreFactory;
-import fr.istic.groupimpl.synthesizer.util.SignalUtil;
 
 /**
  * @authors GroupImpl
