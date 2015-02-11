6170b98ac1cc63487db56a85f485249d9732dab4
gdbhub <gdbhub@users.noreply.github.com>
2015-02-11 11:09:55 +0100

    MIXER (fonction mute non terminee) LINE IN

---
+++ Synthesizer/src/main/java/fr/istic/groupimpl/synthesizer/mixer/ModelMixer.java
@@ -0,0 +1,88 @@
+package fr.istic.groupimpl.synthesizer.mixer;
+
+import java.util.Collection;
+
+import com.jsyn.ports.UnitInputPort;
+import com.jsyn.ports.UnitOutputPort;
+import com.jsyn.ports.UnitPort;
+import com.jsyn.unitgen.UnitGenerator;
+
+import fr.istic.groupimpl.synthesizer.component.ModelComponent;
+import fr.istic.groupimpl.synthesizer.io.architecture.Module;
+import fr.istic.groupimpl.synthesizer.mixer.jsyn.Mixer;
+
+public class ModelMixer extends ModelComponent {
+
+	final Integer NumberOfInputPort = 4;
+	private Mixer mixer;
+	
+	public ModelMixer() {
+		super();
+		mixer = new Mixer(NumberOfInputPort);
+	}
+
+	/**
+	 * Get the number of input port
+	 * 
+	 * @return Integer
+	 */
+	public Integer getNumberOfInputPort() {
+		return NumberOfInputPort;
+	}
+
+	
+	@Override
+	public UnitGenerator getUnitGenerator() {
+		return mixer;
+	}
+
+	@Override
+	public Collection<UnitPort> getAllPorts() {
+		return mixer.getPorts();
+	}
+	
+	/**
+	 * Set an attenuation to the output signal of the index input
+	 * @param index
+	 * 		Input index
+	 * @param dbValue
+	 */
+	public void setAttenuation(Integer index, double dbValue) {
+		mixer.setAttenuation(index, dbValue);
+	}
+
+	/**
+	 * Set Mute to the output signal of the index input
+	 * @param index
+	 * 		Input index
+	 * @param value
+	 *     true|false
+	 */
+	public void setMute(Integer index, boolean value) {
+		mixer.setMute(index, value);
+	}
+	
+	/**
+	 * Get the jsyn output port.
+	 * @return UnitOutputPort
+	 */
+	public UnitOutputPort getOutputPort() {
+		return mixer.getOutput();
+	}
+	
+	/**
+	 * Get the jsyn input port.
+	 * @param index
+	 * 		Input index
+	 * @return
+	 */
+	public UnitInputPort getInputPort(Integer index) {
+		return mixer.getInput(index);
+	}
+
+	@Override
+	public Module getModule() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
