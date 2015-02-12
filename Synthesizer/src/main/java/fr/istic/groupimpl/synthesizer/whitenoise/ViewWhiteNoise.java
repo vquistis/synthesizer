afdef6dff4d272d32bd918024b128afcf5274359
Anthony LHOMME <lhomme.anthony@gmail.com>
2015-02-12 09:56:40 +0100

    Bruit blanc : externalisation top

--- Synthesizer/src/main/java/fr/istic/groupimpl/synthesizer/whitenoise/ViewWhiteNoise.java
+++ Synthesizer/src/main/java/fr/istic/groupimpl/synthesizer/whitenoise/ViewWhiteNoise.java
@@ -7,8 +7,10 @@ import javafx.beans.property.DoubleProperty;
 import javafx.beans.property.SimpleDoubleProperty;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
 import fr.istic.groupimpl.synthesizer.component.ControllerComponent;
 import fr.istic.groupimpl.synthesizer.component.ViewComponent;
@@ -16,7 +18,7 @@ import fr.istic.groupimpl.synthesizer.component.ViewComponent;
 public class ViewWhiteNoise extends ViewComponent implements Initializable {
 
 	@FXML private Pane rootModulePane;
-	@FXML private ImageView closeModuleFx;
+	@FXML private GridPane top;
 	@FXML private ImageView output;
 
 	private DoubleProperty outputX = new SimpleDoubleProperty(0);
@@ -25,6 +27,8 @@ public class ViewWhiteNoise extends ViewComponent implements Initializable {
 
 	@Override
 	public void initialize(URL location, ResourceBundle resource) {
+		((Label) top.lookup("#titleModule")).setText("Briut blanc");
+		
 		addPort(output, outputX, outputY);
 		
 		// implementation of controller
@@ -32,7 +36,7 @@ public class ViewWhiteNoise extends ViewComponent implements Initializable {
 		// Listener output
 		output.addEventHandler(MouseEvent.MOUSE_CLICKED, (e) -> controller.handleViewOutputClick(outputX, outputY));
 		// Listener close module
-		closeModuleFx.addEventHandler(MouseEvent.MOUSE_CLICKED, (e) -> {
+		top.lookup("#closeModuleFx").addEventHandler(MouseEvent.MOUSE_CLICKED, (e) -> {
 			cleanupPorts();			
 			controller.handleViewClose();
 			Pane parent = (Pane) rootModulePane.getParent();
